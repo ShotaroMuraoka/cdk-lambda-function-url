084ab35ae23672d9b63b9e3e5e140233e2c77eda
ShotaroMuraoka <shotaro1987@hotmail.com>
2022-12-12 23:44:24 +0900

    Initial commit

---
+++ test/lambda-without-api-gateway.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaWithoutApiGateway from '../lib/lambda-without-api-gateway-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-without-api-gateway-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaWithoutApiGateway.LambdaWithoutApiGatewayStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
