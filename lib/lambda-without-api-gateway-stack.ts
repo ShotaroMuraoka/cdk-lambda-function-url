cc093c3e32ec073db0307ba3073a6e3a4db8f95e
ShotaroMuraoka <shotaro1987@hotmail.com>
2022-12-14 00:08:24 +0900

    add: cdk code

--- lib/lambda-without-api-gateway-stack.ts
+++ lib/lambda-without-api-gateway-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class LambdaWithoutApiGatewayStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn =  new lambda.Function(this, 'LambdaFunction', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'app.lambda_handler',
+      code: lambda.Code.fromAsset('./lambda'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaWithoutApiGatewayQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fnUrl = fn.addFunctionUrl({
+      // authType: lambda.FunctionUrlAuthType.NONE,
+    });
+    new cdk.CfnOutput(this, 'TheUrl', {
+      value: fnUrl.url,
+    });
   }
 }
